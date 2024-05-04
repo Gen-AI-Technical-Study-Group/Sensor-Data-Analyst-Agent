bf44744ddc014b6be6ecef270766098f596ac7c8
Santanu Sarker <santanu.sarker@ibm.com>
2024-05-03 20:15:24 -0400

    add ability to upload csv

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import openai
+import pandas as pd
+from openai import OpenAI
 import os
 
 from dotenv import load_dotenv
@@ -7,3 +8,14 @@ from dotenv import load_dotenv
 load_dotenv(".env")
 
 st.title("ChatGPT Clone")
+
+# initialize openAI client
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
+uploaded_file = st.file_uploader("Upload your CSV")
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.text("Uploaded Data Sample")
+    df = pd.read_csv(df.head())
+    st.text("Uploaded Data Stats")
+    st.write(df.describe())
